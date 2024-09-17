779e398ae3cd028857975b3e1ca772796e911753
Adam <bahrcr7@gmail.com>
2024-09-17 13:45:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-/* ===================================================================
- * Flare 1.0.0 - Main JS
- *
- * ------------------------------------------------------------------- */
-
 (function($) {
 
     "use strict";
@@ -12,12 +7,6 @@
                 mailChimpURL   : ''   // mailchimp url
                 };
     const $WIN = $(window);
-
-
-    // Add the User Agent to the <html>
-    // will be used for IE10/IE11 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; rv:11.0))
-    // const doc = document.documentElement;
-    // doc.setAttribute('data-useragent', navigator.userAgent);
 
 
    /* preloader
